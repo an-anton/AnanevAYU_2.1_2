940f9b8b224483cf7ca35a0d6a26d8e950af6ae0
Ananev Anton <an-anton88@mail.ru>
2021-10-19 13:00:25 +0300

    Initial Commit

---
+++ AnanevAYU_2.1_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnanevAYU_2.1_2
+//
+//  Created by Anton Anan'eV on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
