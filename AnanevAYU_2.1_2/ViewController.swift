b44fd15df958d109ce7ac471401d84ee97f963f2
Ananev Anton <an-anton88@mail.ru>
2021-10-19 18:55:28 +0300

    сделано все кроме кнопки переключения

--- AnanevAYU_2.1_2/ViewController.swift
+++ AnanevAYU_2.1_2/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewRed: UIView!
+    
+    @IBOutlet var viewYellow: UIView!
+    
+    @IBOutlet var viewGreen: UIView!
+    
+    @IBOutlet var buttonOutlet: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewRed.layer.cornerRadius = viewRed.frame.width / 2
+        viewRed.alpha = 0.3
+        
+        viewYellow.layer.cornerRadius = viewYellow.frame.width / 2
+        viewYellow.alpha = 0.3
+        
+        viewGreen.layer.cornerRadius = viewGreen.frame.width / 2
+        viewGreen.alpha = 0.3
+        buttonOutlet.layer.cornerRadius = buttonOutlet.frame.height / 4
+        
+        buttonOutlet.setTitle("START", for: .normal)
+        
     }
 
-
+    @IBAction func button(_ sender: Any) {
+        
+        buttonOutlet.setTitle("NEXT", for: .normal)
+        viewRed.alpha = 1
+        
+    }
+    
 }
 
