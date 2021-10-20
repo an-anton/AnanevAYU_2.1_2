7297a1f46e1241385cd123d20d3b60a087448cf1
Ananev Anton <an-anton88@mail.ru>
2021-10-20 17:57:16 +0300

    end

--- AnanevAYU_2.1_2/ViewController.swift
+++ AnanevAYU_2.1_2/ViewController.swift
@@ -17,29 +17,20 @@ class ViewController: UIViewController {
     
     @IBOutlet var buttonOutlet: UIButton!
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         viewRed.layer.cornerRadius = viewRed.frame.width / 2
         viewRed.alpha = 0.3
         
-        
-        
         viewYellow.layer.cornerRadius = viewYellow.frame.width / 2
         viewYellow.alpha = 0.3
-        
         
         viewGreen.layer.cornerRadius = viewGreen.frame.width / 2
         viewGreen.alpha = 0.3
         
-        
         buttonOutlet.layer.cornerRadius = buttonOutlet.frame.height / 4
-        
         buttonOutlet.setTitle("START", for: .normal)
-        
     }
 
     @IBAction func button(_ sender: Any) {
@@ -63,8 +54,6 @@ class ViewController: UIViewController {
             viewGreen.alpha = 0.3
             viewYellow.alpha = 0.3
         }
-        
     }
-    
 }
 
